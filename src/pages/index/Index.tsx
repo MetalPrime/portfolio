e464fd800c63d91cfeef9d7ce2cf54b30be6e128
JhonSGIzquierdo <metalprime57@gmail.com>
2026-01-22 18:24:27 -0500

    visuals update info

--- src/pages/index/Index.tsx
+++ src/pages/index/Index.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import './Index.css';
-import Typed from 'react-typed';
 import { ParallaxProvider } from "react-scroll-parallax";
 import { AdvancedBannerTop } from '../../components/Banner/Banner';
+import { Pillars } from '../../components/Pillars/Pillars'
+import { Projects } from '../../components/Projects/Projects'
+import { Footer } from '../../components/Footer/Footer'
 
 interface Index {
 
@@ -10,48 +12,12 @@ interface Index {
 
 export const Index: React.FC<Index> = () => {
 
-    const talkAbout: string[] = [
-        'React',
-        'JavaScript',
-        'TypeScript',
-        'SCSS',
-        'Express.js',
-        'Node.js',
-        'Java',
-        'RESTful APIs',
-        'Arduino',
-        'CSS',
-        'HTML',
-    ];
-
-
     return (
         <ParallaxProvider>
-            <AdvancedBannerTop/>
-
-            
-{/*             <section className='Index'>
-            //aqui va el navigation
-                <h1>¡Qué tranza! I'm Jhon Sebastian Garcia Izquierdo</h1>
-                <p>
-                    A student of Interactive Media Design with a focus on digital
-                    product development hand in hand with research and programming.
-                    I want to make products that make people's lives easier and help
-                    us grow as a species. You can tell me about {''}
-                    <Typed
-                        loop
-                        typeSpeed={80}
-                        backSpeed={20}
-                        strings={talkAbout}
-                        smartBackspace
-                        backDelay={1000}
-                        loopCount={0}
-                        showCursor
-                        cursorChar="|"
-                    />
-                    .
-                </p>
-            </section> */}
+            <AdvancedBannerTop />
+            <Pillars />
+            <Projects />
+            <Footer />
         </ParallaxProvider>
 
     );
